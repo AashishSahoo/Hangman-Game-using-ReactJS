5eb8915bb6cbd88ac6750c029d49603acef1e222
ashish sahoo <ashishsahoo0013@gmail.com>
2023-11-18 19:06:03 +0530

    Hangman-Game

--- src/App.js
+++ src/App.js
@@ -1,24 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Word from "./components/Word";
+import Figure from "./components/Figure";
+import WrongLetters from "./components/WrongLetters";
+import Header from "./components/header";
+import { showNotification as show } from "./Helper/helper";
+import Notification from "./components/Notification";
+import Popup from "./components/Popup";
+// Rainbow
+
+const words = [
+  "application",
+  "Lighthouse",
+  "Library",
+  "Guitar",
+  "Watermelon",
+  "Chocolate",
+  "Adventure",
+  "Butterfly",
+  "Umbrella",
+  "Cupcake",
+  "Keyboard",
+  "Universe",
+  "Detective",
+  "Sandwich",
+  "Mountain",
+  "history",
+  "interface",
+  "wizard",
+  "programming",
+];
+
+let selectedWord = words[Math.floor(Math.random() * words.length)];
+
+let playable = true;
 
 function App() {
+  const [playable, setPlayable] = useState(true);
+  const [correctLetters, setCorrectLetter] = useState([]);
+  const [wrongLetters, setwrongLetters] = useState([]);
+  const [showNotification, setShowNotification] = useState(false);
+
+  useEffect(() => {
+    const handlekeydown = (event) => {
+      const { key, keyCode } = event;
+      if (playable && keyCode >= 65 && keyCode <= 90) {
+        const letter = key.toLowerCase();
+
+        if (selectedWord.includes(letter)) {
+          if (!correctLetters.includes(letter)) {
+            setCorrectLetter((currentLetters) => [...currentLetters, letter]);
+          } else {
+            show(setShowNotification);
+          }
+        } else {
+          if (!wrongLetters.includes(letter)) {
+            setwrongLetters((wrongLetters) => [...wrongLetters, letter]);
+          } else {
+            show(setShowNotification);
+          }
+        }
+      }
+    };
+    window.addEventListener("keydown", handlekeydown);
+    return () => window.removeEventListener("keydown", handlekeydown);
+  }, [correctLetters, wrongLetters, playable]);
+
+  function playAgain() {
+    setPlayable(true);
+
+    // empty arrays
+    setCorrectLetter([]);
+    setwrongLetters([]);
+
+    const random = Math.floor(Math.random() * words.length);
+    selectedWord = words[random];
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <div className="game-container">
+        <Figure wrongLetters={wrongLetters} />
+        <WrongLetters wrongLetters={wrongLetters} />
+        <Word selectedWord={selectedWord} correctLetters={correctLetters} />
+      </div>
+      <Popup
+        correctLetters={correctLetters}
+        wrongLetters={wrongLetters}
+        selectedWord={selectedWord}
+        setPlayable={setPlayable}
+        playAgain={playAgain}
+      />
+      <Notification showNotification={showNotification} />
+    </>
   );
 }
 
